ebdb3e7589fc9c11cc736e5c9f6d6d511afbdcd8
Joseph ochefu sunday <abublessing76@gmail.com>
2025-12-29 13:11:39 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="relative w-full mt-3 px-2">
+    <IconsSearch class="text-gray-400 absolute left-4 top-1/2 -translate-y-1/2 w-5 h-5" />
+    <input
+      type="text"
+      placeholder="Search on Jshop"
+      class="w-full rounded-lg bg-gray-100 text-gray-700 py-3 pl-9 pr-4 text-sm focus:outline-none focus:ring-1 focus:ring-primary"
+    />
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
